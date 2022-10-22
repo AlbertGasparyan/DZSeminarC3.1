240b4e2011b6f79e81865d987226fb8c10c5ccf5
Albert <mona-m1@ya.ru>
2022-10-22 16:32:18 +0600

    Complate dz

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine();
+
+Console.WriteLine("Введите число, соответстующему дню недели, по порядку от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine()??"0");
+
+string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string txtResult = "";
+
+if (dayNumber > 5) txtResult = $". Ура! Это выходной день!";
+else txtResult = $", рабочий день.";
+
+Console.WriteLine();
+Console.WriteLine($"{dayNumber}-й день недели - это {Days[dayNumber - 1]}{txtResult}");
+Console.WriteLine();
--- dz1.1/Program.cs
+++ dz1.1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine()??"0");
+
+if(number>99 && number<999)
+{
+    int result=(number/10) %10;
+    Console.WriteLine($"В числе {number} вторым симоволом является - {result}");
+}
+else
+{
+    Console.WriteLine($"Число {number} не явлеяется трёхзначным числом");
+}
